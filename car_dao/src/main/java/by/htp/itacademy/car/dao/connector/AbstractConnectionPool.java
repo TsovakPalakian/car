20d13021e8b4724e92d5c372ee594fcf524b37ba
Tsovak Palakian <tsovakpalakian@gmail.com>
2017-12-22 15:38:13 +0300

    autocommit all files

--- car_dao/src/main/java/by/htp/itacademy/car/dao/connector/AbstractConnectionPool.java
+++ car_dao/src/main/java/by/htp/itacademy/car/dao/connector/AbstractConnectionPool.java
@@ -15,9 +15,23 @@ public abstract class AbstractConnectionPool {
 	protected void initResource(String configFileName, String urlParam, String loginParam, 
 			String passwordParam, String driverNameParam) 
 					throws DatabaseConnectionException {
+		
 		if (configFileName == null) {
 			throw new DatabaseConnectionException("The configuration file name can not be null");
 		}
+		if (urlParam == null) {
+			throw new DatabaseConnectionException("The connection URL can not be null");
+		}
+		if (loginParam == null) {
+			throw new DatabaseConnectionException("The connection login can not be null");
+		}
+		if (passwordParam == null) {
+			throw new DatabaseConnectionException("The connection password can not be null");
+		}
+		if (driverNameParam == null) {
+			throw new DatabaseConnectionException("The connection driver name can not be null");
+		}
+		
 		ResourceBundle rb = ResourceBundle.getBundle(configFileName);
 
 		url = rb.getString(urlParam);
@@ -34,6 +48,10 @@ public abstract class AbstractConnectionPool {
 	}
 	
 	protected void fillingConnectionPool(ConcurrentHashMap<Connection, Boolean> connPool, Long size) throws DatabaseConnectionException {
+		
+		if (connPool == null || size == null) {
+			throw new DatabaseConnectionException("The connection pool size can not be null");
+		} 
 		
 		for (int i = connPool.size(); i < size; i++) {
 			try {
