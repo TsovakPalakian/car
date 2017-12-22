77ca0432f3aa5f8a1be8908e47be50dce4968f84
Tsovak Palakian <tsovakpalakian@gmail.com>
2017-12-22 19:28:13 +0300

    autocommit all files

--- car_web/src/main/java/by/htp/itacademy/car/web/util/ResponseParameter.java
+++ car_web/src/main/java/by/htp/itacademy/car/web/util/ResponseParameter.java
@@ -1,5 +1,22 @@
 package by.htp.itacademy.car.web.util;
 
-public class ResponseParameter {
+import java.io.Serializable;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@SuppressWarnings("serial")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResponseParameter implements Serializable {
+	
+	private String pageResponse;
+	private boolean stateResponse;
+	
+	public void chageResponse(String pageResponse, boolean stateResponse) {
+		this.pageResponse = pageResponse;
+		this.stateResponse = stateResponse;
+	}
 }
