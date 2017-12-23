3d4e8d381ac131c4ec5784168e1807478d6ecf36
Tsovak Palakian <tsovakpalakian@gmail.com>
2017-12-23 03:59:17 +0300

    autocommit all files

---
+++ car_web/src/main/java/by/htp/itacademy/car/web/command/impl/LogInAction.java
@@ -0,0 +1,16 @@
+package by.htp.itacademy.car.web.command.impl;
+
+import javax.servlet.http.HttpServletRequest;
+
+import by.htp.itacademy.car.web.command.Action;
+import by.htp.itacademy.car.web.util.ResponseParameter;
+
+public class LogInAction implements Action {
+
+	@Override
+	public ResponseParameter execute(HttpServletRequest request) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
