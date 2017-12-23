aca0387e324446efd8754fcdbedf2cd51ee07ef4
Tsovak Palakian <tsovakpalakian@gmail.com>
2017-12-23 04:29:17 +0300

    autocommit all files

--- car_web/src/main/java/by/htp/itacademy/car/web/command/impl/LogInAction.java
+++ car_web/src/main/java/by/htp/itacademy/car/web/command/impl/LogInAction.java
@@ -7,10 +7,20 @@ import by.htp.itacademy.car.web.util.ResponseParameter;
 
 public class LogInAction implements Action {
 
-	@Override
-	public ResponseParameter execute(HttpServletRequest request) {
-		// TODO Auto-generated method stub
-		return null;
+	private LogInAction() {
+		
 	}
 
+	private static class Singletone {
+		private static final LogInAction INSTANCE = new LogInAction();
+	}
+	
+	public static LogInAction getInstance() {
+		return Singletone.INSTANCE;
+	}
+	
+	@Override
+	public ResponseParameter execute(HttpServletRequest request) {
+		return null;
+	}
 }
