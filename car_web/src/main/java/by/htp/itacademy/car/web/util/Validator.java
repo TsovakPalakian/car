57c44bc4f50f49b621930a6132aa9b1fb5958240
Tsovak Palakian <tsovakpalakian@gmail.com>
2018-01-03 11:55:17 +0300

    autocommit all files

---
+++ car_web/src/main/java/by/htp/itacademy/car/web/util/Validator.java
@@ -0,0 +1,12 @@
+package by.htp.itacademy.car.web.util;
+
+public class Validator {
+	private Validator() {};
+	
+	private static final String LOGIN_REGEX = "[A-z0-9]{3,20}";
+    private static final String PASSWORD_REGEX = "((?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\\!\\@\\#\\$\\%\\^\\&\\*]).{8,100})";
+	private static final String STRING_REGEX = "[A-Z]{1}[a-z]{0,100}";
+    private static final String EMAIL_REGEX = "^[a-zA-Z0-9.,_%+-]+@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,4}$";
+    private static final String PASSPORT_REGEX = "[A-Z]{2}[0-9]{7}";
+    private static final String PHONE_NUMBER_REGEX = "[\\+]{1}[0-9]{2,4}[0-9]+";
+}
