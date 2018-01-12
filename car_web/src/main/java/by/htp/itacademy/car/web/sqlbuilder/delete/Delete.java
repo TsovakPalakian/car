af7f95cf59a8c23c08127552be7981148e534c53
Tsovak Palakian <tsovakpalakian@gmail.com>
2018-01-12 19:53:53 +0300

    autocommit all files

--- car_web/src/main/java/by/htp/itacademy/car/web/sqlbuilder/delete/Delete.java
+++ car_web/src/main/java/by/htp/itacademy/car/web/sqlbuilder/delete/Delete.java
@@ -1,5 +1,21 @@
 package by.htp.itacademy.car.web.sqlbuilder.delete;
 
-public class Delete {
+import by.htp.itacademy.car.domain.entity.Entity;
+import by.htp.itacademy.car.web.sqlbuilder.Query;
+import by.htp.itacademy.car.web.sqlbuilder.QueryBuilder;
 
+public final class Delete extends QueryBuilder {
+	
+	private Query query;
+	private Entity entity;
+	
+	public Delete(Query query) {
+		this.query = query;
+		this.query.append("DELETE ");
+	}
+	
+	public Delete(Query query, Entity entity) {
+		this(query);
+		this.entity = entity;
+	}
 }
