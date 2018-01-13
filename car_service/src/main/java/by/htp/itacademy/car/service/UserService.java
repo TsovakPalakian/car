b2a6ad3a17d7a0cc97aa9734906a8f778fe422fc
Tsovak Palakian <tsovakpalakian@gmail.com>
2018-01-13 19:25:13 +0300

    autocommit all files

--- car_service/src/main/java/by/htp/itacademy/car/service/UserService.java
+++ car_service/src/main/java/by/htp/itacademy/car/service/UserService.java
@@ -1,9 +1,20 @@
 package by.htp.itacademy.car.service;
 
+import java.util.List;
+
 import by.htp.itacademy.car.domain.entity.User;
+import by.htp.itacademy.car.service.exception.ServiceException;
 import by.htp.itacademy.car.service.exception.ServiceNoSuchUserException;
 
 public interface UserService {
 	
-	void logIn(User user) throws ServiceNoSuchUserException;
+	User logIn(User user) throws ServiceNoSuchUserException;
+	
+	void signUp(User user) throws ServiceException;
+
+	void removeUser(User user) throws ServiceException;
+	
+	void updateUser(User user) throws ServiceException;
+	
+	<V> List<User> getUser(V value) throws ServiceNoSuchUserException;
 }
