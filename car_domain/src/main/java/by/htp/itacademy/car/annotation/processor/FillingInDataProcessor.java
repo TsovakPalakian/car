9e0b2881743132ebcd0c4d7da111def37c5b29fc
TsovakPalakian <tsovakpalakian@gmail.com>
2018-02-09 02:13:33 +0300

    autocommit all files

--- car_domain/src/main/java/by/htp/itacademy/car/annotation/processor/FillingInDataProcessor.java
+++ car_domain/src/main/java/by/htp/itacademy/car/annotation/processor/FillingInDataProcessor.java
@@ -3,21 +3,38 @@ package by.htp.itacademy.car.annotation.processor;
 import java.util.Set;
 
 import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.ProcessingEnvironment;
 import javax.annotation.processing.RoundEnvironment;
 import javax.annotation.processing.SupportedAnnotationTypes;
-import javax.annotation.processing.SupportedSourceVersion;
 
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.TypeElement;
 
+import com.sun.tools.javac.processing.JavacProcessingEnvironment;
+import com.sun.tools.javac.tree.TreeMaker;
+
 @SupportedAnnotationTypes(value = FillingInDataProcessor.ANNOTATION_TYPE)
-@SupportedSourceVersion(SourceVersion.RELEASE_8)
 public class FillingInDataProcessor extends AbstractProcessor {
 
-	public static final String ANNOTATION_TYPE = "by.htp.itacademy.car.annotation.processor.FillingInDataProcessor";
+	public static final String ANNOTATION_TYPE = "by.htp.itacademy.car.annotation.FillingInData";
+	
+	private JavacProcessingEnvironment javacProcessingEnv;
+	private TreeMaker maker;
+
+	@Override
+	public void init(ProcessingEnvironment procEnv) {
+		super.init(procEnv);
+		this.javacProcessingEnv = (JavacProcessingEnvironment) procEnv;
+		this.maker = TreeMaker.instance(javacProcessingEnv.getContext());
+	}
+	
 	@Override
 	public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
 		return false;
 	}
-
+	
+	@Override
+	public SourceVersion getSupportedSourceVersion() {
+		return SourceVersion.latestSupported();
+	}
 }
